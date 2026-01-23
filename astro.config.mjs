02ad004317282e33ae8a78a1cba7b8737abbfd0f
Anyi Liu <anyiliu@oakland.edu>
2026-01-23 17:39:02 -0500

    Configure base path for Project Page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://anyiliu-mi.github.io',
-    base: '/',
+    base: '/anyiliu.github.io/',
     trailingSlash: 'ignore',
 });
