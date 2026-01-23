012e19c4095114435e616c3987dd3b1c878bf0fb
Anyi Liu <anyiliu@oakland.edu>
2026-01-23 17:11:50 -0500

    Configure deployment for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://anyiliu.github.io',
+    site: 'https://anyiliu-mi.github.io',
     base: '/',
     trailingSlash: 'ignore',
 });
